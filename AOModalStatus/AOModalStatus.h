3da1b2e44e20604655800241573cac2e3dc1e9c9
Mateus Marques <marques.m05@gmail.com>
2018-07-10 08:59:16 -0300

    First commit

---
+++ AOModalStatus/AOModalStatus.h
@@ -0,0 +1,19 @@
+//
+//  AOModalStatus.h
+//  AOModalStatus
+//
+//  Created by Mateus Marques on 10/07/18.
+//  Copyright © 2018 Mateus Marques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AOModalStatus.
+FOUNDATION_EXPORT double AOModalStatusVersionNumber;
+
+//! Project version string for AOModalStatus.
+FOUNDATION_EXPORT const unsigned char AOModalStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AOModalStatus/PublicHeader.h>
+
+
---
+++ AOModalStatus/AOModalStatusView.swift
@@ -0,0 +1,16 @@
+//
+//  AOModalStatusView.swift
+//  AOModalStatus
+//
+//  Created by Mateus Marques on 10/07/18.
+//  Copyright © 2018 Mateus Marques. All rights reserved.
+//
+
+import UIKit
+
+class AOModalStatusView: UIView {
+    @IBOutlet weak var statusImage: UIImageView!
+    @IBOutlet weak var headlineLabel: UILabel!
+    @IBOutlet weak var subheadLabel: UILabel!
+    
+}
